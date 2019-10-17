9bfaafb3cc1fea5c774e9f4d9bee408a28d1c838
jeremy perry <jeremy.m.perry@gmail.com>
2019-10-17 16:59:38 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const playlist = {artistName: "song1"};
 
 function updatePlaylist(playlist, artistName, songTitle){
@@ -9,4 +10,10 @@ function updatePlaylist(playlist, artistName, songTitle){
 function removeFromPlaylist(playlist, artistName){
   delete playlist[artistName];
   return playlist;
+=======
+const playlist = {artist1: "song1"};
+
+function updatePlaylist(playlist, artistName, songTitle){
+ return Object.assign({}, playlist, { [artistName] : songTitle});
+>>>>>>> bb41ce23b7d9d91a459587a424991dd3b81c6caa
 }
